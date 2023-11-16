751513e98e22596c01d3c1425939bb27c5341dbf
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-16 22:27:29 +0100

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
-
+import React from 'react';
+import './App.css';
+import Header from '../src/Components/Header/Header';
+import Phonesection from './Components/PhoneSection/Phonesection';
+import Risktolerance from './Components/Risktolerance/Risktolerance';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Low from './Components/Low/Low';
+import Medium from './Components/Medium/Medium';
+import High from './Components/High/High';
 
 function App() {
+  
   return (
-    <div className="App">
+    <Router>
+      <div>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Phonesection />} />
+          <Route path="/Low" element={<Low />} />
+          <Route path="/Medium" element={<Medium />} />
+          <Route path="/High" element={<High />} />
+        </Routes>
+        <Risktolerance/>
+      </div>
       
-    </div>
+    </Router>
+    
   );
 }
 
 export default App;
+
